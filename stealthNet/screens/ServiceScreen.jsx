f8b527a9f848d94b9aaf413ea428bdc8e0beefc5
Infinite-Concept <ifenowoifesegun@gmail.com>
2024-07-18 13:20:13 -0400

    create screen for difference UI

---
+++ stealthNet/screens/ServiceScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ServiceScreen = () => {
+  return (
+    <View>
+      <Text>ServiceScreen</Text>
+    </View>
+  )
+}
+
+export default ServiceScreen
+
+const styles = StyleSheet.create({})
---
+++ stealthNet/screens/TermsScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TermsScreen = () => {
+  return (
+    <View>
+      <Text>TermsScreen</Text>
+    </View>
+  )
+}
+
+export default TermsScreen
+
+const styles = StyleSheet.create({})
